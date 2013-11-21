121c0617148ad1d38295175b9b29be147cb3ba03
michitaro <michitaro.nike@gmail.com>
2013-11-21 13:53:05 +0900

    raw2fits

---
+++ README.md
@@ -0,0 +1,15 @@
+usage
+-----
+    ./raw2fits -o out.fits in.cr2
+    ds9 &
+    xpaset -p ds9 rgb red   ; xpaset -p ds9 file out.fits'[0]'
+    xpaset -p ds9 rgb green ; xpaset -p ds9 file out.fits'[1]'
+    xpaset -p ds9 rgb blue  ; xpaset -p ds9 file out.fits'[2]'
+
+
+requirements
+------------
+  * sfitsio
+    * http://www.ir.isas.jaxa.jp/~cyamauch/sli/index.html
+  * libraw
+    * http://www.libraw.org
---
+++ raw2fits.cpp
@@ -0,0 +1,81 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sli/fitscc.h>
+#include <libraw/libraw.h>
+
+
+using namespace sli;
+
+
+static void read_raw(fitscc &fits, const char *filename);
+
+
+int main(int argc, char *argv[]) {
+    const char *output_file = NULL,
+               *input_file;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "o:")) != -1) {
+        switch (opt) {
+            case 'o':
+                output_file = optarg;
+                break;
+            default:
+                goto argument_error;
+        }
+    }
+    if (output_file == NULL || optind != argc - 1) {
+        argument_error:
+            fprintf(stderr, "usage: %s [-o OUTPUT] INPUT\n", argv[0]);
+            return 1;
+    }
+    input_file = argv[optind];
+
+    fitscc fits;
+    read_raw(fits, input_file);
+    fits.write_stream(output_file);
+
+    return 0;
+}
+
+
+void read_raw(fitscc &fits, const char *filename) {
+    LibRaw iProcessor;
+    iProcessor.open_file(filename);
+
+    int width  = iProcessor.imgdata.sizes.width  / 2,
+        height = iProcessor.imgdata.sizes.height / 2;
+
+    iProcessor.unpack();
+    iProcessor.raw2image();
+
+    fits.append_image("CHANNEL_R", 0, FITS::FLOAT_T)
+        .append_image("CHANNEL_G", 0, FITS::FLOAT_T)
+        .append_image("CHANNEL_B", 0, FITS::FLOAT_T);
+
+    mdarray_float &r = fits.image(fits.length() - 3).float_array(),
+                  &g = fits.image(fits.length() - 2).float_array(),
+                  &b = fits.image(fits.length() - 1).float_array();
+
+    r.resize_2d(width, height);
+    g.resize_2d(width, height);
+    b.resize_2d(width, height);
+
+    /*
+     * +---+
+     * |R|G|
+     * +-+-+
+     * |G|B|
+     * +-+-+
+     */
+    for (int y = 0;  y < height;  y++) {
+        for (int x = 0;  x < width;  x++) {
+            r(x, y) = iProcessor.imgdata.image[(2*y  )*(2*width) + 2*x  ][0];
+            b(x, y) = iProcessor.imgdata.image[(2*y+1)*(2*width) + 2*x+1][2];
+            g(x, y) = 0.5 * (iProcessor.imgdata.image[(2*y  )*(2*width) + 2*x+1][1] +
+                             iProcessor.imgdata.image[(2*y+1)*(2*width) + 2*x  ][3]);
+        }
+    }
+
+    iProcessor.recycle();
+}
